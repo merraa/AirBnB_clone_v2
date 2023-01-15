37d6215996ed52f8cbb84eaa8ad7314684be3616
meron <104758118+merraa@users.noreply.github.com>
2023-01-15 20:55:51 +0300

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
-from flask import Flask, escape
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    '''
-    Hello route
-    '''
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hbnb():
-    '''
-    HBNB route
-    '''
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    '''
-    /c/<text> route
-    '''
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-@app.route('/python/<text>')
-@app.route('/python/')
-def python_text(text='is cool'):
-    '''
-    /python/<text> route
-    '''
-    return 'Python {}'.format(text.replace('_', ' '))
-
+"""Starts a Flask web application
+"""
+from flask import Flask
 
 if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """Display “C ” followed by the value of
+        the text variable (replace underscore _
+        symbols with a space)
+        """
+        return 'C ' + text.replace('_', ' ')
+
+    @app.route('/python/')
+    @app.route('/python/<text>', strict_slashes=False)
+    def python(text="is cool"):
+        """Display “Python ”, followed by the value of
+        the text variable (replace underscore _
+        symbols with a space )
+        """
+        return 'Python ' + text.replace('_', ' ')
+
+    app.run('0.0.0.0'

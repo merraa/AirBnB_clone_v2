b23bc173374414f734166e7745ebf8287349c8a5
meron <104758118+merraa@users.noreply.github.com>
2023-01-15 20:57:58 +0300

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,65 +1,19 @@
 #!/usr/bin/python3
-from flask import Flask, escape, render_template
+"""Starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    '''
-    Hello route
-    '''
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hbnb():
-    '''
-    HBNB route
-    '''
-    return 'HBNB:'
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    '''
-    /c/<text> route
-    '''
-    return 'C {}'.format(escape(text.replace('_', ' ')))
-
-
-@app.route('/python', defaults={'text': 'is cool'})
-@app.route('/python/<text>')
-def python_text(text):
-    '''
-    /python/<text> route
-    '''
-    return 'Python {}'.format(escape(text.replace('_', ' ')))
-
-
-@app.route('/number/<n>')
-def number_route(n):
-    '''
-    /number/<n> route
-    '''
-    return '{} is a number'.format(int(n))
-
-
-@app.route('/number_template/<n>')
-def number_template(n):
-    '''
-    /number_template/<n> route
-    '''
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def even_or_odd(n):
-    '''
-    /number_odd_or_even/<n> route
-    '''
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Display a HTML page only if n is an integer
+    """
+    parity = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
 
 
 if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+    app.run('0.0.0.0'

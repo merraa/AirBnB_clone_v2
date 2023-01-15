9dcf886ad4a232d2dbf47ca2c4f1d3307f6a0a54
meron <104758118+merraa@users.noreply.github.com>
2023-01-15 20:58:49 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,73 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
-app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    '''
-    Lists states from db
-    '''
-    state_dict = storage.all('State')
-    state_list = []
-
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('7-states_list.html', state_list=state_list)
-
-
-@app.teardown_appcontext
-def teardown_app(e):
-    '''
-    teardown app context
-    '''
-    storage.close()
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """Display “C ” followed by the value of
+        the text variable (replace underscore _
+        symbols with a space)
+        """
+        return 'C ' + text.replace('_', ' ')
+
+    @app.route('/python/')
+    @app.route('/python/<text>', strict_slashes=False)
+    def python(text="is cool"):
+        """Display “Python ”, followed by the value of
+        the text variable (replace underscore _
+        symbols with a space )
+        """
+        return 'Python ' + text.replace('_', ' ')
+
+    @app.route('/number/<int:n>', strict_slashes=False)
+    def number(n):
+        """Display “n is a number” only if n is an integer
+        """
+        return str(n) + ' is a number'
+
+    @app.route('/number_template/<int:n>', strict_slashes=False)
+    def number_template(n):
+        """Display a HTML page only if n is an integer
+        """
+        return render_template('5-number.html', n=n)
+
+    @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+    def number_odd_or_even(n):
+        """Display a HTML page only if n is an integer
+        """
+        parity = 'even' if n % 2 == 0 else 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+    @app.route('/states_list', strict_slashes=False)
+    def states_list():
+        """Display a HTML page of the States
+        """
+        states = storage.all(State).values()
+        return render_template('7-states_list.html', states=states)
+
+    @app.teardown_appcontext
+    def teardown_db(error):
+        """Closes the database again at the end of the request.
+        """
+        storage.close()
+
+    app.run('0.0.0.0')

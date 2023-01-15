ad8003b5dffd8de9c95ab1bffd1970cee7666c00
meron <104758118+merraa@users.noreply.github.com>
2023-01-15 21:41:18 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Starts a Flask application related to HBNB. """
+
+from os import getenv
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -6,39 +9,21 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_state():
-    '''
-    Lists cities by the state
-    it is in
-    '''
-    state_dict = storage.all('State')
-    state_list = []
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('8-cities_by_states.html', state_list=state_list)
-
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    '''
-    Lists states from db
-    '''
-    state_dict = storage.all('State')
-    state_list = []
-
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('7-states_list.html', state_list=state_list)
-
-
 @app.teardown_appcontext
-def teardown_app(e):
-    '''
-    teardown app context
-    '''
+def teardown_db(exception):
+    """Closes the database session after each request."""
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+        Flask route at /cities_by_states.
+        Displays the list of the Cities in the database with their states.
+    """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
